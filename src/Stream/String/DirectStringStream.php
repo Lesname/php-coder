dd253aaa981759d9608b3e7379b0c1af38d2d9bb
mkroeders <mkroeders@gmail.com>
2025-12-14 22:47:20 +0100

    Added file string stream, DirectStringStream is mostly for testing purposes

--- src/Stream/String/DirectStringStream.php
+++ src/Stream/String/DirectStringStream.php
@@ -4,15 +4,9 @@ declare(strict_types=1);
 namespace LesCoder\Stream\String;
 
 use Override;
-use LesCoder\Stream\String\Exception\ExpectedExactly;
-use LesCoder\Stream\String\Exception\ExpectedKeyword;
 
-final class DirectStringStream implements StringStream
+final class DirectStringStream extends AbstractStringStream
 {
-    private int $line = 1;
-
-    private int $column = 1;
-
     public function __construct(private string $input)
     {}
 
@@ -23,85 +17,9 @@ final class DirectStringStream implements StringStream
     }
 
     #[Override]
-    public function matchesExactly(string $input): bool
-    {
-        return $this->isActive() && $this->current(mb_strlen($input)) === $input;
-    }
-
-    #[Override]
-    public function matchesKeyword(string $keyword): bool
-    {
-        $length = mb_strlen($keyword);
-        $current = $this->current($length + 1);
-        $quotedKeyword = preg_quote($keyword);
-
-        return $this->isActive()
-            && preg_match("#^{$quotedKeyword}([^a-z\d_\$]$|$)#i", $current) === 1;
-    }
-
-    #[Override]
-    public function skipWhitespace(): void
-    {
-        while ($this->isActive()) {
-            if (trim($this->current()) !== '') {
-                break;
-            }
-
-            $this->next();
-        }
-    }
-
-    #[Override]
     public function next(int $size = 1): void
     {
-        for ($step = 1; $step <= $size; $step += 1) {
-            if ($this->current() === PHP_EOL) {
-                $this->line += 1;
-                $this->column = 1;
-            } else {
-                $this->column += 1;
-            }
-
-            $this->input = mb_substr($this->input, 1);
-        }
-    }
-
-    /**
-     * @throws ExpectedExactly
-     */
-    #[Override]
-    public function expectExactly(string $string): void
-    {
-        if (!$this->matchesExactly($string)) {
-            throw new ExpectedExactly(
-                $string,
-                $this->current(mb_strlen($string)),
-            );
-        }
-
-        $this->next(mb_strlen($string));
-    }
-
-    /**
-     * @throws ExpectedKeyword
-     */
-    #[Override]
-    public function expectKeyword(string $keyword): void
-    {
-        if (!$this->matchesKeyword($keyword)) {
-            throw new ExpectedKeyword(
-                $keyword,
-                $this->current(mb_strlen($keyword)),
-            );
-        }
-
-        $this->next(mb_strlen($keyword));
-    }
-
-    #[Override]
-    public function isActive(): bool
-    {
-        return !$this->isEnd();
+        $this->input = mb_substr($this->input, $size);
     }
 
     #[Override]
---
+++ test/Stream/String/FileStringStreamTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LesCoderTest\Stream\String;
+
+use LesCoder\Stream\String\FileStringStream;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(FileStringStream::class)]
+class FileStringStreamTest extends TestCase
+{
+    public function testSimpleStream(): void
+    {
+        $stream = new FileStringStream(__DIR__ . '/stub/file-string-stream.test.txt');
+
+        self::assertTrue($stream->isActive());
+        self::assertSame('L', $stream->current());
+//
+        self::assertSame('Lo', $stream->current(2));
+
+        $stream->next(2);
+        self::assertSame('rem', $stream->current(3));
+        $stream->next(3);
+
+        self::assertSame(PHP_EOL, $stream->current());
+        $stream->next();
+
+        self::assertSame('Ipsum', $stream->current(5));
+        $stream->next(5);
+
+        self::assertFalse($stream->isActive());
+    }
+}
